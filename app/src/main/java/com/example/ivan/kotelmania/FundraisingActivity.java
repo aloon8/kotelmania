18169d9726dd52a2cd19c7329f7d90e472d5c528
adirz101 <adirz101@gmail.com>
2019-06-14 15:49:53 +0300

    add activity
    add activity

--- app/src/main/java/com/example/ivan/kotelmania/FundraisingActivity.java
+++ app/src/main/java/com/example/ivan/kotelmania/FundraisingActivity.java
@@ -1,21 +1,59 @@
 package com.example.ivan.kotelmania;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class FundraisingActivity extends AppCompatActivity {
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
+public class FundraisingActivity extends AppCompatActivity {
+    static int Th = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fundraising);
         Button buttonname = (Button) findViewById(R.id.button) ;
-        EditText editText = (EditText) findViewById(R.id.add_heading) ;
-        TextView  textView = (TextView) findViewById(R.id.textView) ;
+        final EditText editText = (EditText) findViewById(R.id.add_heading) ;
+        final TextView textView = (TextView) findViewById(R.id.textView) ;
+        Th = readFromInternalStorage();
+        textView.setText("Fundraising: " + Th);
+        buttonname.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int temp = Integer.parseInt(editText.getText().toString());
+                Th = readFromInternalStorage();
+                Th += temp;
+                textView.setText("Fundraising: " + Th);
+                saveToInternalStorage(Th);
 
-//        buttonname.setOnClickListener();
+            }
+        });
+    }
+    public void saveToInternalStorage(int Fun) {
+        try {
+            FileOutputStream output = openFileOutput("text.txt",MODE_PRIVATE);
+            output.write(Fun);
+            output.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public int readFromInternalStorage(){
+        try{
+            FileInputStream input = openFileInput("text.txt");
+            int r;
+            r=input.read();
+            input.close();
+            return r;
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return -1;
     }
 }
