7f7833a96bbd3a60e09d09611793cc8c49d3ebc2
adirz101 <adirz101@gmail.com>
2019-06-14 15:46:44 +0300

    add Fundraising Activity
    add Fundraising Activity

--- app/src/main/java/com/example/ivan/kotelmania/FundraisingActivity.java
+++ app/src/main/java/com/example/ivan/kotelmania/FundraisingActivity.java
@@ -2,6 +2,9 @@ package com.example.ivan.kotelmania;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class FundraisingActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class FundraisingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fundraising);
+        Button buttonname = (Button) findViewById(R.id.button) ;
+        EditText editText = (EditText) findViewById(R.id.add_heading) ;
+        TextView  textView = (TextView) findViewById(R.id.textView) ;
+
+//        buttonname.setOnClickListener();
     }
 }
